6818c520453d5b29ed7432c94cd4b877ac48c638
liuyueshang0604 <861662019@qq.com>
2024-12-06 17:11:52 +0800

    mybatis dynamic sql 测试

--- src/main/java/com/cn/easyexcel/mapper/StudentMapper.java
+++ src/main/java/com/cn/easyexcel/mapper/StudentMapper.java
@@ -5,14 +5,15 @@ import org.apache.ibatis.annotations.InsertProvider;
 import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.dynamic.sql.insert.render.MultiRowInsertStatementProvider;
 import org.mybatis.dynamic.sql.util.SqlProviderAdapter;
+import org.mybatis.dynamic.sql.util.mybatis3.CommonInsertMapper;
 
 /**
  * @author 86166
  * @date 2024-12-06 11:06
  */
 @Mapper
-public interface  StudentMapper {
+public interface  StudentMapper extends CommonInsertMapper<Student> {
 
-    @InsertProvider(type= SqlProviderAdapter.class, method="insertMultiple")
-    int insertMultiple(MultiRowInsertStatementProvider<Student> insertStatement);
+    //@InsertProvider(type= SqlProviderAdapter.class, method="insertMultiple")
+    //int insertMultiple(MultiRowInsertStatementProvider<Student> insertStatement);
 }
